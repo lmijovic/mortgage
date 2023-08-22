e9851f3e46d0cd340a691a82f59b1fefc9b34263
lmijovic <liza.mijovic@cern.ch>
2023-08-22 17:32:46 +0100

    rate and inflation study, improve mortgage script

--- scripts/get_mtg_into.py
+++ scripts/get_mtg_into.py
@@ -1,3 +1,40 @@
 import mortgage
-m=mortgage.Mortgage(interest=0.0625, amount=300000, months=30*12, term_years = 2, arrangement=999.)
+import argparse
+import os
+
+default_amount = 300000.
+default_rate = 0.05
+default_years = 30
+default_term = 0
+# booking fee
+default_arr = -1.
+
+parser = argparse.ArgumentParser(description="",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--amount", default=default_amount, help="amount ", type = float, dest="amount")
+parser.add_argument("--rate", default=default_rate, help="rate ", type = float, dest="rate")
+parser.add_argument("--years", default=default_years, help="years ", type = int, dest="years")
+parser.add_argument("--term", default=default_term, help="term ", type = int, dest="term")
+parser.add_argument("--arr", default=default_arr, help="arr ", type = float, dest="arr")
+
+args = parser.parse_args()
+
+## here arg name is identical to destination 
+print("\nExample command:\n")
+command = "python " + os.path.basename(__file__)
+for var in vars(args):
+    command += " --"+var
+    command += "="+str(vars(args)[var])
+print(command)
+print("\n")
+
+amount = vars(args)["amount"]
+rate = vars(args)["rate"]
+years = vars(args)["years"]
+term = vars(args)["term"]
+arr = vars(args)["arr"]
+
+
+m=mortgage.Mortgage(interest=rate, amount=amount, months=years*12, term_years = term, arrangement=arr)
+
 mortgage.print_summary(m)
--- scripts/plot_rate_profile.py
+++ scripts/plot_rate_profile.py
@@ -5,32 +5,37 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
 import argparse
-
+import os 
 
 date_format = '%d-%m-%Y'
 default_start = "01-01-1000"
 default_end = "31-01-3000"
+default_inflation = True
 
-
-
-parser = argparse.ArgumentParser(description="",
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("--start", default=default_start, help="start date "+date_format, dest="start")
-parser.add_argument("--end", default=default_end, help="end date "+date_format, dest="end")
+parser = argparse.ArgumentParser()
+parser.add_argument('--start', default=default_start, help="start date d-m-Y", dest="start")
+parser.add_argument('--end', default=default_end, help="end date d-m-Y", dest="end")
+parser.add_argument('--inflation', default=default_inflation, help="plot inflation ", dest="inflation")
 args = parser.parse_args()
 
-#print(args)
-#config = vars(args)
-#print(config)
+## here arg name is identical to destination 
+print("\nExample command:\n")
+command = "python " + os.path.basename(__file__)
+for var in vars(args):
+    command += " --"+var
+    command += "="+str(vars(args)[var])
+print(command)
+print("\n")
+
 date_start =datetime.strptime(vars(args)["start"],date_format)
 date_end =datetime.strptime(vars(args)["end"],date_format)
+inflation = vars(args)["inflation"]
 print("plotting data for:", date_start, date_end)
 
 plt.rcParams.update({'font.size': 16})
 
 headers = ['Date','Value']
-#df = pd.read_csv('virusi.txt',names=headers)
-df = pd.read_csv('../data/BOE_interest_rate.csv',names=headers, comment='#')
+df = pd.read_csv('../data/BOE_interest_rate_Aug23.csv',names=headers, comment='#')
 df['Date']= pd.to_datetime(df['Date'])
 
 print('----- Working with rates file ----- ')
@@ -43,12 +48,41 @@ df = df.sort_values(by=['Date'])
 df = df[(df['Date'] > date_start)]
 df = df[(df['Date'] <= date_end)]
 
-df.plot(x='Date',y='Value',legend=None)
+ax = df.plot(x='Date',y='Value',legend=True,label="BoE rate")
+
+hlines = (0,2,3,4,5,6,7,10)
+for line in hlines:
+    ax.axhline(y=line,color='lightgray',linestyle='--')
+
+vlines = ("01-01-2000","01-01-2005","01-01-2010","01-01-2015","01-01-2020")
+for line in vlines:
+    line_date = x=datetime.strptime(line,date_format)
+    if (line_date > date_start and line_date < date_end):
+        ax.axvline(x=datetime.strptime(line,date_format),
+                   color='lightgray',linestyle='--')
 
 plt.xlabel('',horizontalalignment='right', x=1.0)
 plt.ylabel('Rate',horizontalalignment='right', y=1.0)
 
+
+if (inflation):
+    df = pd.read_csv('../data/inflation/cpi_aug23.csv',names=headers, comment='#')
+    df['Date']= pd.to_datetime(df['Date'], yearfirst=True)
+
+    print('----- Working with inflation file ----- ')
+    print(df.tail(5))
+
+    df = df.sort_values(by=['Date'])
+    
+    # filter acc to range
+    df = df[(df['Date'] > date_start)]
+    df = df[(df['Date'] <= date_end)]
+    df.plot(ax=ax, x='Date',y='Value',color='orange',label="CPI inflation") 
+    #ax = df.plot(x='Date',y='Value',color='orange',legend=None)
+
+plt.legend(frameon=False)
 plt.tight_layout()
-plt.savefig("plot.pdf")
-plt.savefig("plot.png", dpi=900)
+plt.savefig("uk_monetary.pdf")
+plt.savefig("uk_monetary.png", dpi=900)
 plt.show()
+
